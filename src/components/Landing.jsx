cfd1e86e52ebf9f160e54434a322957ec66533f2
heycyntacs <cyntacs.eth@gmail.com>
2022-11-14 22:29:16 +0800

    Canvas Removed

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -13,7 +13,6 @@ const Container = styled.section`
 export default function Landing() {
   return (
     <Container>
-      <canvas id="canvas-bg" className="jsx-9cb0ad42277920de background" data-engine="three.js r139" width="978" height="795"></canvas>
       <h1>Pieces of NFA</h1>
       <Footer />
     </Container>
