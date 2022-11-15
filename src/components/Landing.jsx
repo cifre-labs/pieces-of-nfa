557c87ccea48bf88d0dfe4958fcbe2fc0a374345
heycyntacs <cyntacs.eth@gmail.com>
2022-11-15 14:25:04 +0800

    Added Simple Noise Background

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 import Footer from './Footer';
+import bg from '../assets/ernest-karchmit-UAbg0py6GYQ-unsplash.jpg';
 
 const Container = styled.section`
   height: 100vh;
-  background-color: black;
   color: white;
+  background-color: black;
   display: grid;
   place-content: center;
+  position: relative;
+
+  &::before {   
+      content: "";
+      background-image: url(${bg});
+      background-size: cover;
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      opacity: 0.4;
+      opacity: 0.;
+      height: 100%;
+      width: 100%;
+  }
 `;
+
+const Header = styled.h1`
+  z-index: 2;
+`
 
 export default function Landing() {
   return (
     <Container>
-      <h1>Pieces of NFA</h1>
+      <Header>Pieces of NFA</Header>
       <Footer />
     </Container>
   );
